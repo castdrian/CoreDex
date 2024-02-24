6f7070081ec5e9330d2a29382c11984dec47f139
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2024-02-24 18:13:39 +0100

    Initial Commit

---
+++ CoreDex/CoreDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDexApp.swift
+//  CoreDex
+//
+//  Created by Adrian Castro on 24.02.24.
+//
+
+import SwiftUI
+
+@main
+struct CoreDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
