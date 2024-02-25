9e7b4953f4432989c5b7040546271768c014f828
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2024-02-25 09:41:11 +0100

    feat: image prediction

--- CoreDex/ContentView.swift
+++ CoreDex/ContentView.swift
@@ -10,14 +10,27 @@ import Apollo
 import PkmnApi
 
 let apolloClient = ApolloClient(url: URL(string: "https://graphqlpokemon.favware.tech/v8")!)
+let imagePredictor = ImagePredictor()
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Button("Request"){
-                apolloClient.fetch(query: GetPokemonByDexNumberQuery(number: 722)) { result in
-                  guard let data = try? result.get().data else { return }
+            Button("test"){
+                DispatchQueue.global(qos: .userInitiated).async {
+                    imagePredictor.classifyImage(UIImage(named: "test")!) { result in
+                        switch result {
+                        case .success(let prediction):
+                            print(prediction.classification)
+                            print(prediction.confidence)
+                        case .failure(let error):
+                            print("Error predicting image: \(error)")
+                        }
+                    }
                 }
+                
+                //                apolloClient.fetch(query: GetPokemonByDexNumberQuery(number: 722)) { result in
+                //                  guard let data = try? result.get().data else { return }
+                //                }
             }
         }
         .padding()
---
+++ CoreDex/CoreML/ImagePredictor.swift
@@ -0,0 +1,109 @@
+//
+//  ImagePredictor.swift
+//  CoreDex
+//
+//  Created by Adrian Castro on 24.02.24.
+//
+
+import Vision
+import UIKit
+
+class ImagePredictor {
+    static func createImageClassifier() -> VNCoreMLModel {
+        let defaultConfig = MLModelConfiguration()
+        
+        let imageClassifierWrapper = try? Gen9(configuration: defaultConfig)
+        
+        guard let imageClassifier = imageClassifierWrapper else {
+            fatalError("App failed to create an image classifier model instance.")
+        }
+        
+        let imageClassifierModel = imageClassifier.model
+        
+        guard let imageClassifierVisionModel = try? VNCoreMLModel(for: imageClassifierModel) else {
+            fatalError("App failed to create a `VNCoreMLModel` instance.")
+        }
+        
+        return imageClassifierVisionModel
+    }
+    
+    private static let imageClassifier = createImageClassifier()
+    
+    struct Prediction {
+        let classification: String
+        let confidence: Float
+    }
+    
+    typealias ImagePredictionHandler = (_ prediction: Prediction?) -> Void
+    
+    private var predictionHandlers = [VNRequest: ImagePredictionHandler]()
+    
+    private func createImageClassificationRequest() -> VNImageBasedRequest {
+        let imageClassificationRequest = VNCoreMLRequest(model: ImagePredictor.imageClassifier,
+                                                         completionHandler: visionRequestHandler)
+        
+        imageClassificationRequest.imageCropAndScaleOption = .centerCrop
+        return imageClassificationRequest
+    }
+    
+    private func makePredictions(for photo: UIImage, completionHandler: @escaping ImagePredictionHandler) throws {
+        guard let orientation = CGImagePropertyOrientation(rawValue: UInt32(photo.imageOrientation.rawValue)) else { return }
+        
+        guard let photoImage = photo.cgImage else {
+            fatalError("Photo doesn't have underlying CGImage.")
+        }
+        
+        let imageClassificationRequest = createImageClassificationRequest()
+        predictionHandlers[imageClassificationRequest] = completionHandler
+        
+        let handler = VNImageRequestHandler(cgImage: photoImage, orientation: orientation)
+        let requests: [VNRequest] = [imageClassificationRequest]
+        
+        try handler.perform(requests)
+    }
+    
+    private func visionRequestHandler(_ request: VNRequest, error: Error?) {
+        guard let predictionHandler = predictionHandlers.removeValue(forKey: request) else {
+            fatalError("Every request must have a prediction handler.")
+        }
+        
+        var topPrediction: Prediction? = nil
+        
+        defer {
+            predictionHandler(topPrediction)
+        }
+        
+        if let error = error {
+            print("Vision image classification error...\n\n\(error.localizedDescription)")
+            return
+        }
+        
+        if request.results == nil {
+            print("Vision request had no results.")
+            return
+        }
+        
+        guard let observations = request.results as? [VNClassificationObservation] else {
+            print("VNRequest produced the wrong result type: \(type(of: request.results)).")
+            return
+        }
+        
+        if let highestConfidenceObservation = observations.max(by: { a, b in a.confidence < b.confidence }) {
+            topPrediction = Prediction(classification: highestConfidenceObservation.identifier,
+                                       confidence: highestConfidenceObservation.confidence)
+        }
+    }
+    
+    func classifyImage(_ image: UIImage, completion: @escaping (Result<Prediction, Error>) -> Void) {
+        do {
+            try self.makePredictions(for: image) { prediction in
+                if let prediction = prediction {
+                    completion(.success(prediction))
+                } else {
+                    completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "No predictions available"])))
+                }
+            }
+        } catch {
+            completion(.failure(error))
+        }
+    }}
